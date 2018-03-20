d9af25a2688a1e5d3e4bd1429a12181879cfa173
Mateus Nakajo <mateusnakajo@gmail.com>
2018-03-20 12:22:39 -0300

    Heatmap

--- base_station/views.py
+++ base_station/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 import logging
 
 from base_station.models import IdentifiedBaseStation
-from optimization.find_best_locations import find_best_locations
+from optimization.find_best_locations import find_best_locations, objective
+
+
+import numpy as np
+import itertools
 
 logger = logging.getLogger(__name__)
 
@@ -33,4 +37,34 @@ class ExampleView(TemplateView):
         bs_coordinates = list(map(lambda bs: [bs.point.x, bs.point.y], bss))
 
         context = {'location': location, 'base_stations': bs_coordinates, 'suggestions': solution}
+        return render(request, self.template_name, context)
+
+class HeatMapView(TemplateView):
+    template_name = 'base_station/heat-map.html'
+    
+    def get(self, request, *args, **kwargs):
+        location = [-46.7302, -23.5572]
+        bounds = ((-46.72, -46.71), (-23.57, -23.56))
+
+        bss = IdentifiedBaseStation.get_base_stations_inside_bounds(bounds[0][0], bounds[1][0], bounds[0][1], bounds[1][1]).filter(radio='GSM')
+
+
+        x = np.linspace(bounds[0][0], bounds[0][1], num=70)
+        y = np.linspace(bounds[1][0], bounds[1][1], num=70)
+        coord = [list(e) for e in itertools.product(x,y)]
+
+        covered_area_by_bs = list(map(lambda bs: bs.covered_area, bss))
+        heatmap = list(map(lambda coord: [coord[1], coord[0], (-objective(covered_area_by_bs, coord))*110*110, ], coord))
+        heatmap = sorted(heatmap, key=lambda x: x[2])
+        threshold = heatmap[-1][2]*(10/100)
+        print(heatmap[-1][2])
+        print(heatmap[0][2])
+
+
+        print(heatmap[-1][2]-heatmap[0][2])
+        print(threshold)
+        filtered_heatmap = list(filter(lambda x: heatmap[-1][2] - x[2] <= threshold, heatmap))
+        bs_coordinates = list(map(lambda bs: [bs.point.x, bs.point.y], bss))
+
+        context = {'location': location, 'base_stations': bs_coordinates, 'heatmap': filtered_heatmap}
         return render(request, self.template_name, context)
--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from base_station.views import index, ExampleView
+from base_station.views import index, ExampleView, HeatMapView
 from .api import router
 
 
@@ -26,5 +26,6 @@ urlpatterns = [
     path('api/', include((router.urls, 'django_project'), namespace='api')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', index, name='index'),
-    path('example/', ExampleView.as_view())
+    path('example/', ExampleView.as_view()),
+    path('heat-map/', HeatMapView.as_view())
 ]
