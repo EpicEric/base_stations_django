65d40b03cc70b860ca1d3f8dd70cfd00fab015b6
Eric Pires <ericpires9@gmail.com>
2018-05-23 16:20:47 -0300

    Change cluster caching to use database models

--- cluster/models.py
+++ cluster/models.py
@@ -1,14 +1,16 @@
-from base_station.models import IdentifiedBaseStation
+from django.contrib.gis.db import models
 from django.db import connection
-from django.contrib.gis.geos import GEOSGeometry, Polygon
-from django.core.cache import caches
+from django.contrib.gis.geos import GEOSGeometry, Point
+from functools import reduce
 import math
 
-cache = caches['db_cache']
+from base_station.models import IdentifiedBaseStation
+
 BS_MODEL = IdentifiedBaseStation
 DB_TABLE = BS_MODEL._meta.db_table
-MAX_CACHE_ZOOM = 16
-CLUSTER_EPS_BASE = 70.0
+MAX_CACHE_ZOOM_SIZE = 16
+CLUSTER_EPS_MAX = 120.0
+CLUSTER_EPS_MIN = 70.0
 
 
 def tile_to_bbox(x_tile, y_tile, zoom_size):
@@ -20,61 +22,108 @@ def tile_to_bbox(x_tile, y_tile, zoom_size):
     return tuple((left_deg, bottom_deg, right_deg, top_deg))
 
 
-class Cluster(object):
-    def __init__(self, point, zoom_size, base_stations):
-        self.point = GEOSGeometry(point)
-        self.zoom_size = zoom_size
-        self.count = len(base_stations)
-        if self.count == 1:
-            self.cgi = base_stations.get().cgi
-        else:
-            self.cgi = ""
+class BaseStationCluster(models.Model):
+    point = models.PointField()
+    zoom_size = models.PositiveIntegerField()
+    count = models.PositiveIntegerField()
+    data = models.CharField(max_length=40, blank=True)
 
     def __str__(self):
         if self.count == 1:
-            return "{} ({})".format(self.point, self.cgi)
+            return "{} ({})".format(self.point, self.data)
         return "{} ({} stations)".format(self.point, self.count)
 
     @property
     def base_stations(self):
-        return BS_MODEL.objects.filter(point__distance_lte=(self.point, Cluster.cluster_eps(self.zoom_size)))
+        return BS_MODEL.objects.filter(point__distance_lte=(self.point, BaseStationCluster.cluster_eps(self.zoom_size)))
 
-    @staticmethod
-    def get_clusters(x_tile, y_tile, zoom_size):
-        if zoom_size <= MAX_CACHE_ZOOM:
-            cluster_list = cache.get_or_set(
-                'cluster/{}/{}/{}'.format(zoom_size, x_tile, y_tile),
-                lambda: Cluster.get_clusters_for_tile(x_tile, y_tile, zoom_size),
-                None)
+    @classmethod
+    def generate_clusters(cls, zoom_size):
+        if not cls.objects.filter(zoom_size=zoom_size).exists():
+            if 0 <= zoom_size < MAX_CACHE_ZOOM_SIZE:
+                print("Generating clusters...")
+                cluster_eps = cls.cluster_eps(zoom_size)
+                kmeans_query = """
+                SELECT
+                    unnest(ST_ClusterWithin(point, %s))
+                FROM
+                    {} WHERE zoom_size = %s;""".format(cls._meta.db_table)
+                with connection.cursor() as cursor:
+                    cursor.execute(kmeans_query, [cluster_eps, zoom_size + 1])
+                    cluster_rows = cursor.fetchall()
+
+                cluster_collections = [GEOSGeometry(row[0]) for row in cluster_rows]
+                total = len(cluster_collections)
+                if not total:
+                    raise ValueError("No clusters found for zoom size {}".format(zoom_size + 1))
+                print("Saving data for {} clusters...".format(total))
+                counter = 0
+                percentage = 0
+                for coll in cluster_collections:
+                    clusters = cls.objects.filter(zoom_size=zoom_size + 1, point__contained=coll)
+                    count = reduce((lambda acc, cl: acc + cl.count), clusters, 0)
+                    data = clusters.first().data if count == 1 else ''
+                    point = Point(
+                        reduce((lambda acc, cl: acc + (cl.point.x * float(cl.count))), clusters, 0.0)
+                        / float(count),
+                        reduce((lambda acc, cl: acc + (cl.point.y * float(cl.count))), clusters, 0.0)
+                        / float(count)
+                    )
+                    cluster = BaseStationCluster(
+                        point=point,
+                        zoom_size=zoom_size,
+                        count=count,
+                        data=data)
+                    cluster.save()
+                    counter += 1
+                    prev_percentage = percentage
+                    percentage = int(100 * counter // total)
+                    if percentage > prev_percentage:
+                        print(" {}% done ({} clusters)".format(percentage, counter))
+
+            elif zoom_size == MAX_CACHE_ZOOM_SIZE:
+                print("Generating clusters...")
+                cluster_eps = cls.cluster_eps(zoom_size)
+                kmeans_query = """
+                SELECT
+                    unnest(ST_ClusterWithin(point, %s))
+                FROM
+                    {};""".format(DB_TABLE)
+                with connection.cursor() as cursor:
+                    cursor.execute(kmeans_query, [cluster_eps])
+                    cluster_rows = cursor.fetchall()
+
+                cluster_collections = [GEOSGeometry(row[0]) for row in cluster_rows]
+                total = len(cluster_collections)
+                print("Saving data for {} clusters...".format(total))
+                counter = 0
+                percentage = 0
+                for coll in cluster_collections:
+                    base_stations = BS_MODEL.objects.filter(point__contained=coll)
+                    count = base_stations.count()
+                    bs = base_stations.first() if count == 1 else None
+                    data = '{} ({})'.format(bs.cgi, bs.radio) if bs else ''
+                    point = coll.centroid
+                    cluster = BaseStationCluster(
+                        point=point,
+                        zoom_size=zoom_size,
+                        count=count,
+                        data=data)
+                    cluster.save()
+                    counter += 1
+                    prev_percentage = percentage
+                    percentage = int(100 * counter // total)
+                    if percentage > prev_percentage:
+                        print(" {}% done ({} clusters)".format(percentage, counter))
+
+            else:
+                raise ValueError("zoom_size must be in the [0, {}] interval".format(MAX_CACHE_ZOOM_SIZE))
+
         else:
-            cluster_list = Cluster.get_clusters_for_tile(x_tile, y_tile, zoom_size)
-        return cluster_list
-
-    @staticmethod
-    def get_clusters_for_tile(x_tile, y_tile, zoom_size):
-        bbox = tile_to_bbox(x_tile, y_tile, zoom_size)
-        cluster_eps = Cluster.cluster_eps(zoom_size)
-        bbox_poly = Polygon.from_bbox(bbox)
-        base_stations = BS_MODEL.objects.filter(point__contained=bbox_poly)
-
-        kmeans_query = """
-        SELECT
-            unnest(ST_ClusterWithin(point, %s))
-        FROM
-            {}
-        WHERE
-            id = ANY(%s);""".format(DB_TABLE)
-        with connection.cursor() as cursor:
-            cursor.execute(kmeans_query, [cluster_eps, [bs.id for bs in base_stations]])
-            cluster_rows = cursor.fetchall()
-
-        cluster_collections = [GEOSGeometry(row[0]) for row in cluster_rows]
-        clusters = list(map(
-            lambda coll: Cluster(coll.centroid, zoom_size, base_stations.filter(point__contained=coll)),
-            cluster_collections))
-        return clusters
+            raise ValueError("There are already clusters for zoom size {}".format(zoom_size))
 
     @staticmethod
     def cluster_eps(zoom_size):
         n = 2.0 ** zoom_size
-        return CLUSTER_EPS_BASE / n
+        return (CLUSTER_EPS_MAX * zoom_size + CLUSTER_EPS_MIN * (MAX_CACHE_ZOOM_SIZE - zoom_size)) \
+               / (MAX_CACHE_ZOOM_SIZE * n)
--- cluster/viewsets.py
+++ cluster/viewsets.py
@@ -1,23 +1,23 @@
-from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
-from base_station.models import IdentifiedBaseStation
-from .models import Cluster
-from .serializers import ClusterSerializer
-
-
-class ClusterViewSet(GenericViewSet):
-    queryset = IdentifiedBaseStation.objects.all().order_by('id')
-    serializer_class = ClusterSerializer
-
-    def list(self, request, *args, **kwargs):
-        # Get tile parameters
-        try:
-            x_tile = int(self.request.query_params['x_tile'])
-            y_tile = int(self.request.query_params['y_tile'])
-            zoom_size = int(self.request.query_params['zoom_size'])
-        except KeyError as e:
-            raise ValueError("Missing parameter: {}".format(e.args[0]))
-
-        cluster_list = Cluster.get_clusters(x_tile, y_tile, zoom_size)
-        serializer = self.get_serializer(cluster_list)
-        return Response(serializer.data)
+# from rest_framework.response import Response
+# from rest_framework.viewsets import GenericViewSet
+# from base_station.models import IdentifiedBaseStation
+# from .models import Cluster
+# from .serializers import ClusterSerializer
+#
+#
+# class ClusterViewSet(GenericViewSet):
+#     queryset = IdentifiedBaseStation.objects.all().order_by('id')
+#     serializer_class = ClusterSerializer
+#
+#     def list(self, request, *args, **kwargs):
+#         # Get tile parameters
+#         try:
+#             x_tile = int(self.request.query_params['x_tile'])
+#             y_tile = int(self.request.query_params['y_tile'])
+#             zoom_size = int(self.request.query_params['zoom_size'])
+#         except KeyError as e:
+#             raise ValueError("Missing parameter: {}".format(e.args[0]))
+#
+#         cluster_list = Cluster.get_clusters(x_tile, y_tile, zoom_size)
+#         serializer = self.get_serializer(cluster_list)
+#         return Response(serializer.data)
