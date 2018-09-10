2965e76ce173f5a7cec86cd39a750305398fb4b2
Eric Pires <ericpires9@gmail.com>
2018-09-10 19:09:00 -0300

    Use back-end clusters directly with front-end MarkerCluster

--- cluster/viewsets.py
+++ cluster/viewsets.py
@@ -10,7 +10,7 @@ from .serializers import BaseStationClusterSerializer, BaseStationUnitSerializer
 
 
 class BaseStationClusterPagination(GeoJsonPagination):
-    page_size = 100
+    page_size = 300
     page_size_query_param = 'page_size'
     max_page_size = 300
 
