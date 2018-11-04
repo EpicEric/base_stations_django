59db67b2b4380ab4c6e44b340c97a97af6eb11ec
Mateus Nakajo <mateusnakajo@gmail.com>
2018-11-04 14:27:14 -0200

    add CORS headers to response

--- django_project/settings.py
+++ django_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'corsheaders',
 
     # API
     'django_filters',
@@ -75,12 +76,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080'
+)
 
 ROOT_URLCONF = 'django_project.urls'
 
