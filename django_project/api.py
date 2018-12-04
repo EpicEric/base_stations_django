32421df0c67b3b444d473ce9361b2c375aa105d3
Mateus Nakajo <mateusnakajo@gmail.com>
2018-12-04 17:48:28 -0200

    implementing a simple endpoint of optimization method

--- django_project/api.py
+++ django_project/api.py
@@ -3,10 +3,11 @@ from rest_framework import routers
 from base_station.viewsets import OperatorViewSet
 from cluster.viewsets import BaseStationClusterViewSet
 from map.viewsets import MapInfoViewSet
-
+from optimization.viewsets import OptimizationViewSet
 
 router = routers.DefaultRouter()
 # router.register(r'base_station', IdentifiedBaseStationViewSet, base_name='base-station')
 router.register(r'cluster', BaseStationClusterViewSet, base_name='cluster')
 router.register(r'operator', OperatorViewSet, base_name='operator')
 router.register(r'map_info', MapInfoViewSet, base_name='map_info')
+router.register(r'optimization', OptimizationViewSet, base_name='optimization')
---
+++ optimization/viewsets.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .find_best_locations import OptimizeLocation
+from base_station.models import IdentifiedBaseStation
+
+class OptimizationViewSet(viewsets.ViewSet):
+    def list(self, request):
+        query_params = request._request.GET.copy()
+        min_lat = float(query_params.pop('min_lat', None)[0])
+        max_lat = float(query_params.pop('max_lat', None)[0])
+        min_long = float(query_params.pop('min_long', None)[0])
+        max_long = float(query_params.pop('max_long', None)[0])
+
+        bounds = ((min_lat, max_lat), (min_long, max_long))
+        bss = IdentifiedBaseStation.get_base_stations_inside_bounds(
+            bounds[0][0], bounds[1][0], bounds[0][1], bounds[1][1])\
+            .filter(radio='GSM')
+        solution = OptimizeLocation.taguchi(bss, 2, bounds)
+
+        solution = [list(s) for s in solution]
+        bs_coordinates = list(map(lambda bs: [bs.point.x, bs.point.y], bss))
+
+        response = {'suggestions': solution}
+        return Response(response)
