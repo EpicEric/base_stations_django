3954bcbf827bd83dd97d0eed61cb979a59681ae8
Mateus Nakajo <mateusnakajo@gmail.com>
2018-12-18 02:18:47 -0200

    decrease number of iterations

--- optimization/viewsets.py
+++ optimization/viewsets.py
@@ -22,7 +22,7 @@ class OptimizationViewSet(viewsets.ViewSet):
         bss = IdentifiedBaseStation.get_base_stations_inside_bounds(
             bounds[1][0] - 1/220, bounds[0][0] - 1/220, bounds[1][1] + 1/220, bounds[0][1] + 1/220)\
             .filter(radio='GSM')
-        solution = OptimizeLocation.random_search(bss, number_erbs, bounds, 400)
+        solution = OptimizeLocation.random_search(bss, number_erbs, bounds, 100)
         solution = [list(s) for s in solution]
         bs_coordinates = list(map(lambda bs: [bs.point.x, bs.point.y], bss))
         response = {'suggestions': solution}
