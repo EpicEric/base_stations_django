10305cd5122966f167d30f1e90f4a2548f3577c6
Mateus Nakajo <mateusnakajo@gmail.com>
2018-12-19 08:15:57 -0200

    random search

--- optimization/viewsets.py
+++ optimization/viewsets.py
@@ -22,7 +22,7 @@ class OptimizationViewSet(viewsets.ViewSet):
         bss = IdentifiedBaseStation.get_base_stations_inside_bounds(
             bounds[1][0] - 1/110, bounds[0][0] - 1/110, bounds[1][1] + 1/110, bounds[0][1] + 1/110)\
             .filter(radio='GSM')
-        solution = OptimizeLocation.taguchi(bss, number_erbs, bounds)
+        solution = OptimizeLocation.random_search(bss, number_erbs, bounds, 100)
         solution = [list(s) for s in solution]
         bs_coordinates = list(map(lambda bs: [bs.point.x, bs.point.y], bss))
         response = {'suggestions': solution}
